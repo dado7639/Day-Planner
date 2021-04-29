27af90f085dfb95ecf13be1c6575cdad6e7cae91
Daniel Doerr <danieldoerr7639@gmail.com>
2021-04-28 20:24:10 -0600

    added date with moment/started saving to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
 // current day displayed at top of calender
 var currentDay = $("#currentDay");
-const today = moment();
-currentDay = today;
+var momentDay = moment().format("MMM Do YYYY");
+currentDay.text(momentDay);
+console.log(currentDay);
 
-console.log(today);
 //each time block is color coded to indicate wether it is in the past, present, or future
 
 // save text to local storage
+var saveBtn = $("saveBtn");
+
+//on click
+saveBtn.addEventListner("click", function (event) {
+  event.preventDefault();
+
+  var description = $("#description");
+  // set user input to local storage
+  localStorage.setItem("description", JSON.stringify(description));
+});
+
+localStorage.getItem("description");

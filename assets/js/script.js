a851227877053928c8e3e1cbe80fd4d7daf0b7e3
Daniel Doerr <danieldoerr7639@gmail.com>
2021-04-28 20:32:41 -0600

    played around with local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,15 @@ console.log(currentDay);
 
 // save text to local storage
 var saveBtn = $("saveBtn");
+var description = $("#description");
 
 //on click
 saveBtn.addEventListner("click", function (event) {
   event.preventDefault();
 
-  var description = $("#description");
   // set user input to local storage
   localStorage.setItem("description", JSON.stringify(description));
 });
 
+// get input saved from local storage and keep it in the text area
 localStorage.getItem("description");

0f6f0e7c43c3ddd50fbad2811dbf537ae81a2edd
Daniel Doerr <danieldoerr7639@gmail.com>
2021-04-29 16:00:47 -0600

    added functions for checking time and saving to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,59 @@
-// current day displayed at top of calender
-var currentDay = $("#currentDay");
-var momentDay = moment().format("MMM Do YYYY");
-currentDay.text(momentDay);
-console.log(currentDay);
+$(document).ready(function () {
+  //write all of our jQuery inside this function
 
-//each time block is color coded to indicate wether it is in the past, present, or future
+  //dispaly the date to the user
+  //first grab a reference to the element that we want the time to be displayed
+  var currentDay = $("#currentDay");
+  //display current date in that variable
+  currentDay.text(moment().format("dddd, MMM Do"));
 
-// save text to local storage
-var saveBtn = $("saveBtn");
-var description = $("#description");
+  //dynamically style the time blocks based on current time
+  //functions
+  //funciotn will be responsible for checking the time and determining what class to add(past present or future)
+  function checkTime() {
+    // grab the current hour using moment js
+    var currentHour = moment().hours();
 
-//on click
-saveBtn.addEventListner("click", function (event) {
-  event.preventDefault();
+    //need to grab the hour for the timeblock
+    // loop through the timeblock hours
+    //each every member of the arry will be passed till there isnt one then it exists out
+    $(".time-block").each(function () {
+      //parse int will take the string and turn it into int
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
 
-  // set user input to local storage
-  localStorage.setItem("description", JSON.stringify(description));
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (blockHour === currentHour) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  checkTime();
+
+  //event handlers
+
+  $(".saveBtn").on("click", function () {
+    //need to capture the information that the user inputed and we need to store that info into local STORAGE
+
+    //need to capture the value of that input and i also need to know what timeblock it is attached to
+    var value = $(this).siblings(".description").val();
+    // attr takes the ID attribute of that parent
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, value);
+  });
+
+  //display the items in local storage to time blocks
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 });
-
-// get input saved from local storage and keep it in the text area
-localStorage.getItem("description");

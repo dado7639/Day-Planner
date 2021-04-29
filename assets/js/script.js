8c8945f0d72d8f170008261548ca315a29d9b4d2
Daniel Doerr <danieldoerr7639@gmail.com>
2021-04-28 19:23:21 -0600

    added time blocks and starter code

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// current day displayed at top of calender
+var currentDay = $("#currentDay");
+const today = moment();
+currentDay = today;
+
+console.log(today);
+//each time block is color coded to indicate wether it is in the past, present, or future
+
+// save text to local storage
